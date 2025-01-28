2b36096448fb903b12425945f77fb299dc7623d4
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-28 12:18:35 +0100

    a

--- assets/js/album.js
+++ assets/js/album.js
@@ -38,7 +38,7 @@ fetch(URL, {
       const songNumberContainer = document.createElement("div");
       songNumberContainer.classList.add("col-1");
       const songNumber = document.createElement("p");
-      songNumber.classList.add("text-white");
+      songNumber.classList.add("text-secondary");
       songNumber.innerText = i + 1; // iterare l'array delle canzoni contenute nella playlist o nell'album
       const titleContainer = document.createElement("div");
       titleContainer.classList.add("col-7");
@@ -49,16 +49,19 @@ fetch(URL, {
       artist.classList.add("text-secondary");
       artist.innerText = albums[i].artist.name; // sostituire con artista
       const durationContainer = document.createElement("div");
-      durationContainer.classList.add("col-4");
+      durationContainer.classList.add("col-4", "d-flex", "justify-content-between");
+      const reproductions = document.createElement("span");
+      reproductions.classList.add("text-secondary");
+      reproductions.innerText = albums[i].rank;
       const duration = document.createElement("h3");
-      duration.classList.add("d-flex", "justify-content-end", "text-white");
+      duration.classList.add("text-secondary");
       duration.innerText = formatDuration(albums[i].duration); //sostituire con durata
       const hr = document.createElement("hr");
       hr.classList.add("text-secondary");
 
-      albumImg = document.getElementById("albumImg");
-      artistName = document.getElementById("artist");
-      songName = document.getElementById("songName");
+      const albumImg = document.getElementById("albumImg");
+      const artistName = document.getElementById("artist");
+      const songName = document.getElementById("songName");
       albumImg.src = albums[0].album.cover_big;
       artistName.innerText = albums[0].artist.name;
       songName.innerText = albums[0].album.title;
@@ -67,6 +70,6 @@ fetch(URL, {
       songRow.append(songNumberContainer, titleContainer, durationContainer, hr);
       songNumberContainer.appendChild(songNumber);
       titleContainer.append(songTitle, artist);
-      durationContainer.appendChild(duration);
+      durationContainer.append(reproductions, duration);
     }
   });
