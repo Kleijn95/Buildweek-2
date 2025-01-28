9a6cf2596e6c7f17aebd78f845f58920a58695fe
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-28 15:42:02 +0100

    lista brani artista

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,3 +1,9 @@
+function formatDuration(seconds) {
+  const minutes = Math.floor(seconds / 60); // Calcola i minuti interi
+  const remainingSeconds = seconds % 60; // Ottieni i secondi rimanenti
+  // Formatta i secondi per avere 2 cifre (es. "05" invece di "5")
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
 const params = new URLSearchParams(window.location.search);
 const artistId = params.get("artistId");
 const URL = "https://striveschool-api.herokuapp.com/api/deezer/artist/" + artistId + "/top?limit=10";
@@ -26,51 +32,40 @@ fetch(URL, {
     const artistName = document.getElementById("artistName");
     artistName.innerText = artist.data[0].artist.name;
 
-    /*  for (i = 0; i < albums.length; i++) {
-        //iterare la playlist o l'album
-        const songsContainer = document.getElementById("songsContainer");
-        const songRow = document.createElement("div");
-        songRow.classList.add("row", "align-items-center");
-        const songNumberContainer = document.createElement("div");
-        songNumberContainer.classList.add("col-1");
-        const songNumber = document.createElement("p");
-        songNumber.classList.add("text-secondary", "text-end", "songNumber", "fw-bold", "mb-2");
-        songNumber.innerText = i + 1; // iterare l'array delle canzoni contenute nella playlist o nell'album
-        const titleContainer = document.createElement("div");
-        titleContainer.classList.add("col-7", "mb-3");
-        const songTitle = document.createElement("h3");
-        songTitle.classList.add("text-white", "mb-0");
-        songTitle.innerText = albums[i].title; //sostituire con titolo canzone
-        const artist = document.createElement("a");
-        artist.classList.add("text-secondary", "text-decoration-none", "artist");
-        artist.href = "./artist.html?artistId=" + albums[i].artist.id;
-        artist.innerText = albums[i].artist.name; // sostituire con artista
-        const durationContainer = document.createElement("div");
-        durationContainer.classList.add("col-4", "d-flex", "justify-content-between");
-        const reproductions = document.createElement("span");
-        reproductions.classList.add("text-secondary");
-        reproductions.innerText = albums[i].rank;
-        const duration = document.createElement("h3");
-        duration.classList.add("text-secondary");
-        duration.innerText = formatDuration(albums[i].duration); //sostituire con durata
-  
-        const albumImg = document.getElementById("albumImg");
-        const artistName = document.getElementById("artist");
-        const songName = document.getElementById("songName");
-        albumImg.src = albums[0].album.cover_big;
-        artistName.innerText = albums[0].artist.name;
-        songName.innerText = albums[0].album.title;
-  
-        document.querySelector(".artistPic").src = data.artist.picture;
-        document.querySelector(".artistPic").alt = data.artist.name;
-        document.querySelector(".tracks").innerText = i + 1 + " brani";
-        document.querySelector(".year").innerText = data.release_date.slice(0, 4);
-        document.querySelector(".duration").innerText = formatDuration(data.duration);
-  
-        songsContainer.appendChild(songRow);
-        songRow.append(songNumberContainer, titleContainer, durationContainer);
-        songNumberContainer.appendChild(songNumber);
-        titleContainer.append(songTitle, artist);
-        durationContainer.append(reproductions, duration);
-      } */
+    for (i = 0; i < artist.data.length; i++) {
+      //iterare la playlist o l'album
+      const songsContainer = document.getElementById("artistTracks");
+      const songRow = document.createElement("div");
+      songRow.classList.add("row", "align-items-center", "d-flex", "pb-3");
+      const songNumberContainer = document.createElement("div");
+      songNumberContainer.classList.add("col-1");
+      const songNumber = document.createElement("p");
+      songNumber.classList.add("text-secondary", "text-end", "songNumber", "fw-bold", "mb-2");
+      songNumber.innerText = i + 1; // iterare l'array delle canzoni contenute nella playlist o nell'album
+      const albumArtContainer = document.createElement("div");
+      albumArtContainer.classList.add("col-2");
+      const albumArt = document.createElement("img");
+      albumArt.src = artist.data[i].album.cover;
+      albumArt.classList.add("img-fluid");
+      const titleContainer = document.createElement("div");
+      titleContainer.classList.add("col", "mb-3");
+      const songTitle = document.createElement("h3");
+      songTitle.classList.add("text-white", "mb-0");
+      songTitle.innerText = artist.data[i].title; //sostituire con titolo canzone
+      const durationContainer = document.createElement("div");
+      durationContainer.classList.add("col-4", "d-flex", "justify-content-between");
+      const reproductions = document.createElement("span");
+      reproductions.classList.add("text-secondary");
+      reproductions.innerText = artist.data[i].rank;
+      const duration = document.createElement("h3");
+      duration.classList.add("text-secondary");
+      duration.innerText = formatDuration(artist.data[i].duration); //sostituire con durata
+
+      songsContainer.appendChild(songRow);
+      songRow.append(songNumberContainer, albumArtContainer, titleContainer, durationContainer);
+      songNumberContainer.appendChild(songNumber);
+      titleContainer.appendChild(songTitle);
+      durationContainer.append(reproductions, duration);
+      albumArtContainer.appendChild(albumArt);
+    }
   });
