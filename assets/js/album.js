794555512538495a3e34326fb608e55add7249ab
MeridjanShoti <meridjanshoti@gmail.com>
2025-01-28 11:18:21 +0100

    modifiche album

--- assets/js/album.js
+++ assets/js/album.js
@@ -6,20 +6,23 @@ for (i = 0; i < 15; i++) {
   const songNumberContainer = document.createElement("div");
   songNumberContainer.classList.add("col-1");
   const songNumber = document.createElement("p");
-  songNumber.classList.add("text-white");
+  songNumber.classList.add("text-secondary");
   songNumber.innerText = i + 1; // iterare l'array delle canzoni contenute nella playlist o nell'album
   const titleContainer = document.createElement("div");
   titleContainer.classList.add("col-7");
   const songTitle = document.createElement("h3");
   songTitle.classList.add("text-white");
   songTitle.innerText = "Titolo canzone"; //sostituire con titolo canzone
-  const artist = document.createElement("h4");
+  const artist = document.createElement("h5");
   artist.classList.add("text-secondary");
   artist.innerText = "Artista sconosciuto"; // sostituire con artista
   const durationContainer = document.createElement("div");
-  durationContainer.classList.add("col-4");
+  durationContainer.classList.add("col-4", "d-flex", "justify-content-between");
+  const reproductions = document.createElement("span");
+  reproductions.classList.add("text-secondary");
+  reproductions.innerText = "897.998"; // sostituire con dato vero
   const duration = document.createElement("h3");
-  duration.classList.add("d-flex", "justify-content-end", "text-white");
+  duration.classList.add("text-secondary");
   duration.innerText = "0:00"; //sostituire con durata
   const hr = document.createElement("hr");
   hr.classList.add("text-secondary");
@@ -28,5 +31,5 @@ for (i = 0; i < 15; i++) {
   songRow.append(songNumberContainer, titleContainer, durationContainer, hr);
   songNumberContainer.appendChild(songNumber);
   titleContainer.append(songTitle, artist);
-  durationContainer.appendChild(duration);
+  durationContainer.append(reproductions, duration);
 }
