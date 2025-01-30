ca1f9cb33afb1fdd57715d0eefce0b4d356a1605
DGSimone99 <simone.digiorgio99@gmail.com>
2025-01-30 14:45:04 +0100

    Responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,88 +1,90 @@
-bannerHide();
-altroChePiace();
-carosello();
-
-document.addEventListener("playlistsLoaded", () => {
-  //Richiama l'evento concluso della sidebar
-  let playlists = JSON.parse(sessionStorage.getItem("playlists"));
-  playlistCard();
-  function playlistCard() {
-    setTimeout(() => {
-      let cardCount = 0;
-      const maxCards = 6;
-
-      playlists.forEach((playlistId) => {
-        if (cardCount >= maxCards) return;
-        const arrayAlbums = playlistId.tracks.data;
-
-        const mainRow = document.getElementById("mainRow");
-        const card = document.createElement("div");
-        card.classList.add("col-4");
-        const innerCard = document.createElement("div");
-        innerCard.classList.add("card", "mb-3", "bg-secondary", "overflow-hidden");
-        const cardRow = document.createElement("div");
-        cardRow.classList.add("row", "g-0");
-        const containerBig = document.createElement("div");
-        containerBig.classList.add("col-md-4");
-        const imgContainer = document.createElement("div");
-        imgContainer.classList.add("d-flex", "flex-wrap", "h-100");
-        imgContainer.style.cursor = "pointer";
-
-        imgContainer.addEventListener("click", function () {
-          window.location.assign(`./album.html?playlistId=${playlistId.id}`);
-        });
-        const img1 = document.createElement("img");
-        img1.classList.add("img-fluid", "w-50", "p-0");
-        img1.alt = `img1 alt`;
-        img1.src = playlistId.picture_xl;
-        const img2 = document.createElement("img");
-
-        img2.classList.add("img-fluid", "w-50", "p-0");
-        img2.alt = `img2 alt`;
-        img2.src = "";
-        const img3 = document.createElement("img");
-        img3.classList.add("img-fluid", "w-50", "p-0");
-        img3.alt = `img3 alt`;
-        img3.src = "./assets/imgs/main/image-10.jpg";
-        const img4 = document.createElement("img");
-        img4.classList.add("img-fluid", "w-50", "p-0");
-        img4.alt = `img4 alt`;
-        img4.src = "./assets/imgs/main/image-10.jpg";
-        for (let index = 0; index < arrayAlbums.length; index++) {
-          img1.src = arrayAlbums[0].album.cover_xl;
-          img2.src = arrayAlbums[1].album.cover_xl;
-          img3.src = arrayAlbums[2].album.cover_xl;
-          img4.src = arrayAlbums[3].album.cover_xl;
-        }
-
-        const outerCardBody = document.createElement("div");
-        outerCardBody.classList.add("col-md-8", "d-flex", "align-items-center");
-        outerCardBody.style.cursor = "pointer";
-        const cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title", "text-white", "text-truncate-multiline");
-        cardTitle.innerText = playlistId.title;
-        mainRow.appendChild(card);
-        card.appendChild(innerCard);
-        innerCard.appendChild(cardRow);
-        cardRow.appendChild(containerBig);
-        containerBig.appendChild(imgContainer);
-        imgContainer.appendChild(img1);
-        imgContainer.appendChild(img2);
-        imgContainer.appendChild(img3);
-        imgContainer.appendChild(img4);
-        cardRow.appendChild(outerCardBody);
-        outerCardBody.appendChild(cardBody);
-        cardBody.appendChild(cardTitle);
-        cardCount++;
-        // outerCardBody.addEventListener("click", function () {
-        //   window.location.assign(`./album.html?albumId=${albums.picture_xl}`);
-        // });
-      });
-    }, 1000);
+function mediaQuery() {
+  if (mediaQueryMd.matches) {
+    bannerHide();
+    carosello();
+  } else {
+    altroChePiace();
   }
-});
+}
+
+playlistCard();
+function playlistCard() {
+  setTimeout(() => {
+    let playlists = JSON.parse(sessionStorage.getItem("playlists"));
+    let cardCount = 0;
+    const maxCards = 6;
+
+    playlists.forEach((playlistId) => {
+      if (cardCount >= maxCards) return;
+      const arrayAlbums = playlistId.tracks.data;
+
+      const mainRow = document.getElementById("mainRow");
+      const card = document.createElement("div");
+      card.classList.add("col-6", "col-md-4");
+      const innerCard = document.createElement("div");
+      innerCard.classList.add("card", "d-flex", "mb-3", "bg-card", "overflow-hidden");
+      innerCard.addEventListener("click", function () {
+        window.location.assign(`./album.html?playlistId=${playlistId.id}`);
+      });
+      const cardRow = document.createElement("div");
+      cardRow.classList.add("row", "g-0");
+      const containerBig = document.createElement("div");
+      containerBig.classList.add("col-md-4", "w-25");
+      const imgContainer = document.createElement("div");
+      imgContainer.classList.add("d-flex", "flex-wrap", "h-100");
+      imgContainer.style.cursor = "pointer";
+
+      const img1 = document.createElement("img");
+      img1.classList.add("img-fluid", "w-50", "p-0");
+      img1.alt = `img1 alt`;
+      img1.src = playlistId.picture_xl;
+      const img2 = document.createElement("img");
+
+      img2.classList.add("img-fluid", "w-50", "p-0");
+      img2.alt = `img2 alt`;
+      img2.src = "";
+      const img3 = document.createElement("img");
+      img3.classList.add("img-fluid", "w-50", "p-0");
+      img3.alt = `img3 alt`;
+      img3.src = "./assets/imgs/main/image-10.jpg";
+      const img4 = document.createElement("img");
+      img4.classList.add("img-fluid", "w-50", "p-0");
+      img4.alt = `img4 alt`;
+      img4.src = "./assets/imgs/main/image-10.jpg";
+      for (let index = 0; index < arrayAlbums.length; index++) {
+        img1.src = arrayAlbums[0].album.cover_xl;
+        img2.src = arrayAlbums[1].album.cover_xl;
+        img3.src = arrayAlbums[2].album.cover_xl;
+        img4.src = arrayAlbums[3].album.cover_xl;
+      }
+
+      const outerCardBody = document.createElement("div");
+      outerCardBody.classList.add("col-md-8", "d-flex", "align-items-center");
+      outerCardBody.style.cursor = "pointer";
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+      const cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title", "text-white", "text-truncate-multiline");
+      cardTitle.innerText = playlistId.title;
+      mainRow.appendChild(card);
+      card.appendChild(innerCard);
+      innerCard.appendChild(cardRow);
+      cardRow.appendChild(containerBig);
+      containerBig.appendChild(imgContainer);
+      imgContainer.appendChild(img1);
+      imgContainer.appendChild(img2);
+      imgContainer.appendChild(img3);
+      imgContainer.appendChild(img4);
+      cardRow.appendChild(outerCardBody);
+      outerCardBody.appendChild(cardBody);
+      cardBody.appendChild(cardTitle);
+      cardCount++;
+      // outerCardBody.addEventListener("click", function () {
+      //   window.location.assign(`./album.html?albumId=${albums.picture_xl}`);
+      // });
+    });
+  }, 500);
+}
 
 function bannerHide() {
   let hide = document.querySelector(".hide");
@@ -120,10 +122,23 @@ function altroChePiace() {
       const albumRaw = document.querySelector("#albumRaw");
 
       const containerRow = document.createElement("div");
-      containerRow.classList.add("row", "d-felx", "gap-1", "justify-content-between");
+      containerRow.classList.add("row", "d-flex", "gap-lg-1", "gap-md-0", "justify-content-between");
       albums.forEach((album, i) => {
         const cardAlbum = document.createElement("div");
-        cardAlbum.classList.add("card", "col-2", "bg-dark", "h-100", "rounded", "p-2", "d-flex", "flex-column", "m-2");
+        cardAlbum.classList.add(
+          "card",
+          "col-4",
+          "col-md-3",
+          "col-lg-2",
+          "me-md-0",
+          "bg-dark",
+          "h-100",
+          "rounded",
+          "p-2",
+          "d-flex",
+          "flex-column",
+          "m-2"
+        );
         cardAlbum.style.cursor = "pointer";
 
         const imgAlbum = document.createElement("img");
--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,12 +1,25 @@
-createHeader();
-playlistNav();
-createAside();
-toggleAside();
+const mediaQueryMd = window.matchMedia("(min-width: 769px)");
+const mediaQuerySm = window.matchMedia("(max-width: 768px)");
+
+mediaQuery();
+
+function mediaQuery() {
+  if (mediaQueryMd.matches) {
+    createHeaderLg();
+    playlistNav();
+    createAside();
+    toggleAside();
+  } else {
+    createHeaderSm();
+    createAsideSm();
+    toggleAsideSm();
+  }
+}
 
 //Header
-function createHeader() {
+function createHeaderLg() {
   let header = document.querySelector("header");
-  header.classList.add("col-2", "px-0", "bg-black", "pt-4");
+  header.classList.add("col-lg-2", "col-md-3", "px-0", "bg-black", "pt-4");
 
   let sidebar = document.createElement("div");
   sidebar.classList.add("d-flex", "flex-column", "h-100", "sidebar");
@@ -173,61 +186,104 @@ function createHeader() {
   sidebar.append(nav, hr, playlistContainer);
   header.appendChild(sidebar);
 }
+
+function createHeaderSm() {
+  let header = document.querySelector("header");
+
+  header.classList.add("fixed-bottom", "bg-headerMobile", "py-2");
+
+  let nav = document.createElement("nav");
+  nav.classList.add("navbar", "container-fluid");
+
+  let divContainer = document.createElement("div");
+  divContainer.classList.add("container-fluid", "d-flex");
+
+  // Home link
+  let homeLink = document.createElement("a");
+  homeLink.classList.add("nav-link", "active", "text-white", "d-flex", "flex-column", "align-items-center");
+  homeLink.href = "./index.html";
+
+  let homeIcon = document.createElement("i");
+  homeIcon.classList.add("fa-solid", "fa-house", "iconNav", "fs-1");
+
+  let homeText = document.createElement("p");
+  homeText.classList.add("mb-0");
+  homeText.textContent = "Home";
+
+  homeLink.appendChild(homeIcon);
+  homeLink.appendChild(homeText);
+
+  let searchLink = document.createElement("a");
+  searchLink.classList.add("nav-link", "px-0");
+  searchLink.href = "#";
+
+  let searchButton = document.createElement("button");
+  searchButton.classList.add("btn", "d-flex", "flex-column", "align-items-center");
+  searchButton.type = "button";
+
+  let searchText = document.createElement("p");
+  searchText.classList.add("text-gray", "d-flex", "flex-column", "align-items-center", "mb-0");
+
+  let searchIcon = document.createElement("i");
+  searchIcon.classList.add("fa", "fa-magnifying-glass", "iconNav", "fs-1");
+
+  let searchLabel = document.createElement("span");
+  searchLabel.classList.add("mb-0");
+  searchLabel.textContent = "Cerca";
+
+  searchText.appendChild(searchIcon);
+  searchText.appendChild(searchLabel);
+  searchButton.appendChild(searchText);
+  searchLink.appendChild(searchButton);
+
+  searchButton.onclick = () => {
+    window.location.assign(`./search.html`);
+  };
+
+  let libraryLink = document.createElement("a");
+  libraryLink.classList.add("nav-link", "text-gray", "px-0", "d-flex", "flex-column", "align-items-center");
+  libraryLink.href = "#";
+
+  let libraryIcon = document.createElement("div");
+  libraryIcon.innerHTML = `
+    <svg data-encore-id="icon" role="img" aria-hidden="true" class="Svg-sc-ytk21e-0 bneLcE e-9541-icon" viewBox="0 0 24 24" style="width: 35px; fill: #abb0ad">
+      <path d="M3 22a1 1 0 0 1-1-1V3a1 1 0 0 1 2 0v18a1 1 0 0 1-1 1zM15.5 2.134A1 1 0 0 0 14 3v18a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V6.464a1 1 0 0 0-.5-.866l-6-3.464zM9 2a1 1 0 0 0-1 1v18a1 1 0 1 0 2 0V3a1 1 0 0 0-1-1z"></path>
+    </svg>
+  `;
+
+  let libraryText = document.createElement("span");
+  libraryText.textContent = "La tua libreria";
+
+  libraryLink.appendChild(libraryIcon);
+  libraryLink.appendChild(libraryText);
+
+  divContainer.appendChild(homeLink);
+  divContainer.appendChild(searchLink);
+  divContainer.appendChild(libraryLink);
+
+  nav.appendChild(divContainer);
+  header.appendChild(nav);
+}
+
 function playlistNav() {
-  //FETCH////////////////////////////////////////////////////////////////
-  const arrayPlaylist = [
-    8454338222, 13015611143, 248297032, 1976454162, 2298075882, 8606835902, 2153050122, 1282495565, 6682665064,
-    1313621735, 1116187241, 733113466,
-  ];
-  let playlists = JSON.parse(sessionStorage.getItem("playlists")) || [];
-
-  let fetchCount = 0;
-
-  // ciclo per popolare asidebar sinistra delle playlist. L'array playlist è chiamato in cima (reminder!! ho dato display none ai placeholder)
-  arrayPlaylist.forEach((playlistId) => {
-    fetch(`https://deezerdevs-deezer.p.rapidapi.com/playlist/${playlistId}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "x-rapidapi-key": "ad4ebc50e8msh21d6de872e740a5p1740a2jsn2f44656a84db",
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-      },
+  fetch("./assets/js/playlists.json")
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error("Impossibile recuperare gli album. Riprova più tardi.");
+      }
     })
-      .then((resp) => {
-        if (resp.ok) {
-          return resp.json();
-        } else {
-          throw new Error("Impossibile recuperare gli album. Riprova più tardi.");
-        }
-      })
-      .then((data) => {
-        if (!playlists.some((pl) => pl.id === data.id)) {
-          playlists.push(data);
-        }
-        fetchCount++;
-      })
-      .catch((error) => {
-        console.error(`Errore per playlist ${playlistId}:`, error);
-      })
-      .finally(() => {
-        if (fetchCount === arrayPlaylist.length) {
-          if (playlists.length > 12) {
-            playlists = playlists.slice(0, 12);
-          }
+    .then((data) => {
+      console.log(data);
+      sessionStorage.setItem("playlists", JSON.stringify(data));
+    })
+    .finally(() => {
+      playlistsBar();
+    });
 
-          sessionStorage.setItem("playlists", JSON.stringify(playlists));
-          console.log("Controllo Console:", playlists);
-
-          document.dispatchEvent(new Event("playlistsLoaded")); //Evento per aspettare che finisca prima di essere caricato, o almeno credo
-        }
-      });
-  });
-  document.addEventListener("playlistsLoaded", () => {
-    if (!playlists) {
-      playlists = [];
-    }
-
-    sessionStorage.setItem("playlists", JSON.stringify(playlists));
+  function playlistsBar() {
+    let playlists = JSON.parse(sessionStorage.getItem("playlists"));
     playlists.forEach((playlistId) => {
       let placeholderPlaylist = document.querySelector(".playlists");
 
@@ -241,13 +297,13 @@ function playlistNav() {
       });
 
       placeholderPlaylist.appendChild(pPlaylist);
-    }, 1000);
-  });
+    });
+  }
 }
-
 //Aside
 function createAside() {
   let aside = document.querySelector("aside");
+  aside.classList.add("col-lg-2", "bg-black");
 
   let container = document.createElement("div");
   container.classList.add("container", "pt-4", "ps-1", "pe-0");
@@ -285,12 +341,12 @@ function createAside() {
                   src="./assets/imgs/search/image-11.jpg"
                 />
                 <div>
-                  <h4 class="mb-0 fw-bold notificationText">Charlie Hookham</h4>
+                  <h4 class="mb-0 fw-bold notificationText overflow-hidden text-truncate">Charlie Hookham</h4>
                   <p class="mb-1 notificationText">In Camera - Yumi Zouma</p>
                   <p><i class="fas fa-music me-1" style="color: #ffffff"></i> EP III</p>
                 </div>
               </div>
-              <p class="time d-none d-lg-block">4 ore</p>
+              <p class="time d-none">4 ore</p>
             </div>
             <div class="d-flex justify-content-between gap-1 mt-3">
               <div class="d-flex gap-2">
@@ -305,7 +361,7 @@ function createAside() {
                   <p><i class="fas fa-music me-1" style="color: #ffffff"></i> Aimed to Kill</p>
                 </div>
               </div>
-              <p class="time d-none d-lg-block">8 ore</p>
+              <p class="time d-none">8 ore</p>
             </div>
             <div class="d-flex justify-content-between gap-1 mt-3">
               <div class="d-flex gap-2">
@@ -320,7 +376,7 @@ function createAside() {
                   <p><i class="fas fa-music me-1" style="color: #ffffff"></i>Twint Badass mood</p>
                 </div>
               </div>
-              <p class="time d-none d-lg-block">3 giorni</p>
+              <p class="time d-none">3 giorni</p>
             </div>
           </div>`;
 
@@ -332,6 +388,118 @@ function createAside() {
 
   container.append(activities, notifications);
   aside.appendChild(container);
+}
+
+function createAsideSm() {
+  let aside = document.querySelector("aside");
+  let body = document.querySelector("body");
+  let div = document.querySelector("aside > div");
+  let username = document.querySelector("main > div > div");
+
+  username.classList.add("d-none");
+
+  aside.classList.add("position-absolute", "text-white");
+  div.innerHTML = `<i class="bi bi-bell fs-1"></i>
+            <i class="bi bi-clock fs-1 mx-4"></i>
+            <i class="bi bi-gear fs-1"></i>`;
+
+  let notify = document.createElement("div");
+  notify.classList.add(
+    "bg-black",
+    "d-none",
+    "position-absolute",
+    "top-0",
+    "start-0",
+    "notify",
+    "w-100",
+    "h-100",
+    "p-4"
+  );
+
+  let container = document.createElement("div");
+  container.classList.add("container", "pt-4", "ps-1", "pe-0");
+
+  let activities = document.createElement("div");
+  activities.classList.add("d-flex", "justify-content-between", "align-items-center");
+
+  let title = document.createElement("h3");
+  title.classList.add("text-white", "mb-0");
+  title.innerText = "Attività amici";
+
+  let controls = document.createElement("div");
+  controls.classList.add("text-secondary", "d-flex", "flex-nowrap");
+
+  let userGroupButton = document.createElement("button");
+  userGroupButton.classList.add("asideBtn", "bg-transparent", "border-0", "d-none", "d-lg-block");
+
+  let userGroupIcon = document.createElement("i");
+  userGroupIcon.classList.add("fa-solid", "fa-user-group", "text-gray", "me-1");
+  userGroupButton.appendChild(userGroupIcon);
+
+  let closeButton = document.createElement("button");
+  closeButton.classList.add("asideBtn", "bg-transparent", "border-0", "closeAside");
+
+  let closeIcon = document.createElement("i");
+  closeIcon.classList.add("fa-solid", "fa-x", "text-gray", "fs-1");
+
+  let notifications = document.createElement("div");
+  notifications.innerHTML = `<div class="container mt-5 text-gray notifications ps-1 pe-0">
+                      <div class="d-flex justify-content-between gap-1">
+                        <div class="d-flex gap-2">
+                          <img
+                            style="width: 35px; height: 35px; border-radius: 50%"
+                            class="img-fluid"
+                            src="./assets/imgs/search/image-11.jpg"
+                          />
+                          <div>
+                            <h4 class="mb-0 fw-bold notificationText overflow-hidden text-truncate">Charlie Hookham</h4>
+                            <p class="mb-1 notificationText">In Camera - Yumi Zouma</p>
+                            <p><i class="fas fa-music me-1" style="color: #ffffff"></i> EP III</p>
+                          </div>
+                        </div>
+                        <p class="time">4 ore</p>
+                      </div>
+                      <div class="d-flex justify-content-between gap-1 mt-3">
+                        <div class="d-flex gap-2">
+                          <img
+                            style="width: 35px; height: 35px; border-radius: 50%"
+                            class="img-fluid"
+                            src="./assets/imgs/search/image-12.jpg"
+                          />
+                          <div>
+                            <h4 class="mb-0 fw-bold notificationText">lightdark02</h4>
+                            <p class="mb-1 notificationText">Aimed to kill - Jared LeMac</p>
+                            <p><i class="fas fa-music me-1" style="color: #ffffff"></i> Aimed to Kill</p>
+                          </div>
+                        </div>
+                        <p class="time">8 ore</p>
+                      </div>
+                      <div class="d-flex justify-content-between gap-1 mt-3">
+                        <div class="d-flex gap-2">
+                          <img
+                            style="width: 35px; height: 35px; border-radius: 50%"
+                            class="img-fluid"
+                            src="./assets/imgs/search/image-15.jpg"
+                          />
+                          <div>
+                            <h4 class="mb-0 fw-bold notificationText">Valeria Traverso</h4>
+                            <p class="mb-1 notificationText">New Kings - Sleeping Wolf</p>
+                            <p><i class="fas fa-music me-1" style="color: #ffffff"></i>Twint Badass mood</p>
+                          </div>
+                        </div>
+                        <p class="time">3 giorni</p>
+                      </div>
+                    </div>`;
+
+  closeButton.appendChild(closeIcon);
+
+  controls.append(userGroupButton, closeButton);
+
+  activities.append(title, controls);
+
+  container.append(activities, notifications);
+  notify.appendChild(container);
+  body.appendChild(notify);
 }
 
 function toggleAside() {
@@ -362,5 +530,18 @@ function toggleAside() {
         hideDiv.classList.add("hideDiv2");
       }
     }
+  });
+}
+
+function toggleAsideSm() {
+  let notifyBtn = document.querySelector(".bi-bell");
+  let notify = document.querySelector(".notify");
+
+  notifyBtn.addEventListener("click", () => {
+    notify.classList.remove("d-none");
+  });
+  let closeAside = document.querySelector(".closeAside");
+  closeAside.addEventListener("click", () => {
+    notify.classList.add("d-none");
   });
 }
